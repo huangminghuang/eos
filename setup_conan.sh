c532f3053fda1085e75a009c50963d9469562ae4
Huang-Ming Huang <huangh@objectcomputing.com>
2019-01-25 10:19:57 -0600

    Allow live output for conan install step

--- setup_conan.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )"
-
-conan remote add conan-community https://api.bintray.com/conan/conan-community/conan
-conan remote add bincrafters https://api.bintray.com/conan/bincrafters/public-conan
-conan remote add huang https://api.bintray.com/conan/huangminghuang/conan
-
-conan install ${SCRIPT_DIR}
